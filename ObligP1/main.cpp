2234bdb5ee512bab46650f2b1a02cb94ffd4db6b
Henriette Brekke Sunde <henriesu@stud.ntnu.no>
2022-01-24 13:29:07 +0100

    Kan legge inn gitt antall tråder

--- ObligP1/main.cpp
+++ ObligP1/main.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
 using namespace std;
 
-void checkIfPrime(int num) {
-    int div = 0;
-
-    for (int i = 1; i <= num; i++){
-        if (num % i == 0) {
-            div++;
+bool checkIfPrime(int num) {
+    for (int i = 2; i <= num/2; i++){
+        if (num % i == 0 && num!=2) {
+            return false;
         }
     }
-    if(div == 2){
-        cout << num << endl;
-        this_thread::sleep_for(1s);
-    }
+    return true;
 }
-void goThroughRange(int low, int high){
-    for(int i = low; i <= high; i++){
-        checkIfPrime(i);
+
+void goThroughRange(int low, int high, vector<int> *primes, int numberOfIntervals){
+    for(int i = low; i <= high; i+= numberOfIntervals){
+        if(checkIfPrime(i)){
+            primes ->emplace_back(i);
+        }
     }
 }
 int main() {
-    int lowerLimit, upperLimit;
+    int lowerLimit, upperLimit, numberOfThreads;
     cout << "Enter lower and upper ranges: ";
     cin >> lowerLimit >> upperLimit;
+    cout << "Enter number of threads: ";
+    cin >> numberOfThreads;
 
-    thread t1(checkIfPrime, lowerLimit);
-    t1.join();
-    thread t2(goThroughRange, lowerLimit, upperLimit);
-    t2.join();
+    vector<int> array[numberOfThreads];
+    vector<thread> threads;
+    vector<int> primes;
+
+    for(int i = 0; i < numberOfThreads; i++){
+        threads.emplace_back(goThroughRange, lowerLimit + i, upperLimit, &array[i], numberOfThreads);
+        cout << "Thread " << i + 1 << ", ID: " << threads.at(i).get_id() << endl;
+    }
+
+    for(auto &thread: threads){
+        thread.join();
+    }
+
+    for(int i = 0; i < numberOfThreads; i++) {
+        primes.insert(primes.end(), array[i].begin(), array[i].end());
+    }
+
+    sort(primes.begin(), primes.end());
+
+    for(int i = 0; i < primes.size(); i++){
+        cout << primes.at(i) << "\n";
+    }
 }
