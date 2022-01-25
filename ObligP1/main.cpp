cfb1550c51655867a680099dbcc0e274a7e07b48
Henriette Brekke Sunde <henriesu@stud.ntnu.no>
2022-01-25 11:12:42 +0100

    La til kommentarer

--- ObligP1/main.cpp
+++ ObligP1/main.cpp
@@ -7,16 +7,17 @@ using namespace std;
 bool checkIfPrime(int num) {
     for (int i = 2; i <= num/2; i++){
         if (num % i == 0 && num!=2) {
-            return false;
+            return false; //Returnerer false dersom num ikke er et primtall
         }
     }
-    return true;
+    return true; //Returnerer true dersom num er et primtall
 }
 
 void goThroughRange(int low, int high, vector<int> *primes, int numberOfIntervals){
     for(int i = low; i <= high; i+= numberOfIntervals){
         if(checkIfPrime(i)){
-            primes ->emplace_back(i);
+            primes ->emplace_back(i); //Dersom i er ett primtall blir primtallet lagt til på slutten i
+            // pointeren til vektoren primes
         }
     }
 }
@@ -32,21 +33,21 @@ int main() {
     vector<int> primes;
 
     for(int i = 0; i < numberOfThreads; i++){
-        threads.emplace_back(goThroughRange, lowerLimit + i, upperLimit, &array[i], numberOfThreads);
-        cout << "Thread " << i + 1 << ", ID: " << threads.at(i).get_id() << endl;
+        threads.emplace_back(goThroughRange, lowerLimit + i, upperLimit, &array[i], numberOfThreads); //Pointeren til vektoren primes(med primtallene), blir lagt til i threads
+        cout << "Thread " << i + 1 << ", ID: " << threads.at(i).get_id() << endl; //Skriver ut tråd-id
     }
 
     for(auto &thread: threads){
-        thread.join();
+        thread.join(); //Kjører de gitte trådene
     }
 
     for(int i = 0; i < numberOfThreads; i++) {
-        primes.insert(primes.end(), array[i].begin(), array[i].end());
+        primes.insert(primes.end(), array[i].begin(), array[i].end()); //Alle primtallene blir lagt til i vektoren primes
     }
 
-    sort(primes.begin(), primes.end());
+    sort(primes.begin(), primes.end()); //Primtallene blir sortert, med C++ sin egen sorteringsmetode
 
     for(int i = 0; i < primes.size(); i++){
-        cout << primes.at(i) << "\n";
+        cout << primes.at(i) << "\n"; //Primtallene blir skrive ut
     }
 }
